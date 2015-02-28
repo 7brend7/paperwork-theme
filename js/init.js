804ed572795cc248c4daa16802d92bbf14fee30e
Borys Anikiyenko <7brend7@gmail.com>
2015-02-28 20:11:49 +0200

    Update mobile view. Some style fixes.

--- js/init.js
+++ js/init.js
@@ -19,6 +19,58 @@ $(function() {
         }
     });
 
-    $('.main .content > .row > div').matchHeight();
+    $('.content .set-height').matchHeight();
     $('.notebook-controls [data-toggle="tooltip"]').tooltip();
+
+    if(matchMedia('(max-width: 991px)').matches) {
+        $('.content .sidebar').parent().addClass('mobile-left-slider');
+        $('.mobile-left-slider').css({
+            'left': '-' + $('body').width() + 'px'
+        });
+
+        var snapper = new Snap({
+            element: $('.mobile-left-slider')[0],
+            maxPosition: $('body').width(),
+            minPosition: '-' + $('body').width()
+        });
+
+
+        $('.mobile-show-navigation .sidebar-toggle').click(function() {
+            snapper.open('left');
+            $('.mobile-show-navigation .sidebar-toggle-hidden').show();
+        });
+
+        $('.mobile-show-navigation .sidebar-toggle-hidden').click(function() {
+            snapper.close();
+            $(this).hide()
+        });
+
+        $('.content .sort-by-row').parent().add($('.content .single-note-content').parent()).wrapAll(
+            $('<div/>').addClass('owl-carousel')
+        );
+
+        $('.owl-carousel').owlCarousel({
+            items: 1,
+            dots: false
+        });
+
+        $(window).resize(function() {
+            var width = $('body').width();
+            $('.mobile-show-navigation .sidebar-toggle-hidden').trigger('click');
+            $('.mobile-left-slider').css({
+                'left': '-' + width + 'px'
+            });
+            snapper.settings({
+                maxPosition: width
+            });
+
+            // Force desktop view... remove mobile features
+            if(matchMedia('(min-width: 992px)').matches) {
+                $('.owl-carousel').data('owlCarousel').destroy();
+                $('.owl-carousel').children().unwrap();
+                $('.owl-stage-outer').children().unwrap();
+                $('.content .sidebar').parent().removeClass('mobile-left-slider').css('left', '');
+            }
+        });
+    }
 });
